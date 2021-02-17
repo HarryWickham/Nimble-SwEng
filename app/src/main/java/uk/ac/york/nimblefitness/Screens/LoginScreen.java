5feedb1e13f477f0418b75db5f1f8bcbf9b19b58
Hadley <hadleythomas26@googlemail.com>
2021-02-17 19:38:40 +0000

    I have linked the app to the Firebase console

--- app/src/main/java/uk/ac/york/nimblefitness/Screens/LoginScreen.java
+++ app/src/main/java/uk/ac/york/nimblefitness/Screens/LoginScreen.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import uk.ac.york.nimblefitness.R;
 
 public class LoginScreen extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
