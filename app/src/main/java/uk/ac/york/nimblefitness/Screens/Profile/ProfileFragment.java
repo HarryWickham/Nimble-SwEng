7c647bee5beb49733a1718e9ea319b9331b33d1f
Harry Wickham <51520004+HarryWickham@users.noreply.github.com>
2021-02-23 22:01:54 +0000

    Redesign on profile fragment with calender changing written date

--- app/src/main/java/uk/ac/york/nimblefitness/Screens/Profile/ProfileFragment.java
+++ app/src/main/java/uk/ac/york/nimblefitness/Screens/Profile/ProfileFragment.java
@@ -3,8 +3,10 @@ package uk.ac.york.nimblefitness.Screens.Profile;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,12 +16,19 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Objects;
 
 import uk.ac.york.nimblefitness.R;
 import uk.ac.york.nimblefitness.Screens.SigninActivity;
 
 public class ProfileFragment extends Fragment {
+
+    private static final String TAG = "log";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,6 +44,24 @@ public class ProfileFragment extends Fragment {
         String[] completed_activity_list = {"Press-ups", "Sit-ups", "Plank", "Crunches"}; //the text that goes in each different list view item
 
         ListView listView = (ListView) view.findViewById(R.id.completed_moves_list); //find the list view from the fragment_settings.xml file
+        CalendarView calendarView = view.findViewById(R.id.profile_calendar);
+        TextView dayNumber = view.findViewById(R.id.profile_date);
+
+        SimpleDateFormat month = new SimpleDateFormat("M");
+        SimpleDateFormat day = new SimpleDateFormat("d");
+        Date currentTime = Calendar.getInstance().getTime();
+        String monthString = month.format(currentTime);
+        String dayString = day.format(currentTime);
+
+        dayNumber.setText(monthText(Integer.parseInt(monthString)) + " " + dayString + datePrefix(Integer.parseInt(dayString)));
+
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                dayNumber.setText(monthText(month+1) + " " + dayOfMonth + datePrefix(dayOfMonth));
+            }
+        });
 
         ArrayAdapter<String> ListViewAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, completed_activity_list); //need to make a 'simple_list_item_1' replacement -> 'settings_list_layout' allow the use of images in the list view like in settings activity example
         listView.setAdapter(ListViewAdapter);
@@ -48,6 +75,41 @@ public class ProfileFragment extends Fragment {
         });
         return view;
     }
+
+    public String monthText(int month){
+        switch (month){
+            case 1: return("January");
+            case 2: return("February");
+            case 3: return("March");
+            case 4: return("April");
+            case 5: return("May");
+            case 6: return("June");
+            case 7: return("July");
+            case 8: return("August");
+            case 9: return("September");
+            case 10: return("October");
+            case 11: return("November");
+            case 12: return("December");
+            default: return("error");
+        }
+    }
+
+    public String datePrefix(int dayOfMonth){
+        switch (dayOfMonth){
+            case 1:
+            case 21:
+            case 31:
+                return ("st");
+            case 2:
+            case 22:
+                return ("nd");
+            case 3:
+            case 23:
+                return ("rd");
+            default: return ("th");
+        }
+    }
+
 }
 
 
