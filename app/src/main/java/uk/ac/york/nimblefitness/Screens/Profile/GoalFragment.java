2ef0770a10ffa1dee3d0874fbffd2dbb1b08d0c4
Elliott Whitfield <elscsi2@hotmail.com>
2021-03-15 18:21:48 +0000

    Profile tabs visually finished, subject to approval

--- app/src/main/java/uk/ac/york/nimblefitness/Screens/Profile/GoalFragment.java
+++ app/src/main/java/uk/ac/york/nimblefitness/Screens/Profile/GoalFragment.java
@@ -8,14 +8,28 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Random;
 
+import pl.pawelkleczkowski.customgauge.CustomGauge;
 import uk.ac.york.nimblefitness.R;
 
 
 public class GoalFragment extends Fragment {
+
+    FirebaseDatabase rootDatabase;
+    DatabaseReference rootReference;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +45,13 @@ public class GoalFragment extends Fragment {
         ListView listView = (ListView) view.findViewById(R.id.todays_moves); //finds where the string array 'moves_to_do' should go
         ArrayAdapter<String> ListViewAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, moves_to_do); //need to make a 'simple_list_item_1' replacement -> 'settings_list_layout' allow the use of images in the list view like in settings activity example
         listView.setAdapter(ListViewAdapter);
+
+        CustomGauge gauge = (CustomGauge) view.findViewById(R.id.gauge);
+        int gaugeValue = 33;
+        gauge.setValue(gaugeValue);//This will eventually be coded such that the completed moves is used in setValue
+        gauge.setEndValue(100);    // and the moves to complete for a day/week is used in setEndValue.
+        TextView gaugeNumber = view.findViewById(R.id.moves_counter);
+        gaugeNumber.setText(String.valueOf(gaugeValue));
 
         String[] quotes = {"You can do it!","Feel the burn.","Time to make some gains.","Time to exercise!"}; //List of quotes which may appear on the 'goals' tab of the profile page
         TextView motivationQuote = view.findViewById(R.id.motivation); //finds where the quote should go in this layout
@@ -52,7 +73,25 @@ public class GoalFragment extends Fragment {
             default:
                 motivationQuote.setText("Improve your health today!");
         }
+
+        TextView userName = view.findViewById(R.id.user_name);
+        rootDatabase = FirebaseDatabase.getInstance();
+        rootReference = rootDatabase.getReference("users");
+        FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        rootReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String user_firstName = snapshot.child(currentFirebaseUser.getUid()).child("firstName").getValue(String.class);
+                String user_lastName = snapshot.child(currentFirebaseUser.getUid()).child("lastName").getValue(String.class);
+
+                userName.setText(user_firstName + " " + user_lastName);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
         return view;
     }
-    //add firebase section so that profile name displays on this tab
 }
