acb0d97eedc835c9cf07a51219a0bf31895cff90
Erion Uka <eu553@york.ac.uk>
2021-04-25 16:38:14 +0100

    updated settings fragment

--- app/src/main/java/uk/ac/york/nimblefitness/Screens/Settings/SettingsFragment.java
+++ app/src/main/java/uk/ac/york/nimblefitness/Screens/Settings/SettingsFragment.java
@@ -12,8 +12,6 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -46,12 +44,6 @@ public class SettingsFragment extends Fragment {
 
         View view; //shows the fragment_settings.xml file in the frame view of the activity_main.xml
         view = inflater.inflate(R.layout.fragment_settings, container, false);
-
-        ActionBar actionBar = ((AppCompatActivity)getActivity()).getSupportActionBar();
-
-        actionBar.setDisplayHomeAsUpEnabled(true);
-
-
 
         String[] settings_list_items = {"Billing Information", "Membership Plan", "Account", "Terms and Conditions", "Logout", "User Details", "HandlerTestActivity"}; //the text that goes in each different list view item
 
@@ -112,9 +104,8 @@ public class SettingsFragment extends Fragment {
                     startActivity(mIntent);
                     break;
                 }
-
             }
-
+            
         });
 
         return view;
