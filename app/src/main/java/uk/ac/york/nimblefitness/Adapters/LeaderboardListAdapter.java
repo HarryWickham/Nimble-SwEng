00f24eac3fafeb95b8b020e5bb05b85a7ab66226
Harry <harrywickham@live.co.uk>
2021-06-05 15:46:12 +0100

    Leaderboard share feature added

--- app/src/main/java/uk/ac/york/nimblefitness/Adapters/LeaderboardListAdapter.java
+++ app/src/main/java/uk/ac/york/nimblefitness/Adapters/LeaderboardListAdapter.java
@@ -6,12 +6,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import uk.ac.york.nimblefitness.HelperClasses.LeaderBoardUserDetails;
+import uk.ac.york.nimblefitness.HelperClasses.ShareService;
 import uk.ac.york.nimblefitness.R;
 
 public class LeaderboardListAdapter extends BaseAdapter {
@@ -53,6 +55,7 @@ public class LeaderboardListAdapter extends BaseAdapter {
             viewHolder.firstNameTV = convertView.findViewById(R.id.leaderboard_first_name);
             viewHolder.scoreTV = convertView.findViewById(R.id.leaderboard_score);
             viewHolder.leaderBoardLayout = convertView.findViewById(R.id.leader_board_layout);
+            viewHolder.leaderBoardShareButton = convertView.findViewById(R.id.leaderboard_share_button);
 
             convertView.setTag(viewHolder);
         } else {
@@ -60,12 +63,22 @@ public class LeaderboardListAdapter extends BaseAdapter {
         }
         if(leaderBoardUserDetails.get(position).isUser()){
             viewHolder.leaderBoardLayout.setBackgroundColor(Color.parseColor("#edb964"));
+            viewHolder.leaderBoardShareButton.setVisibility(View.VISIBLE);
         }else{
             viewHolder.leaderBoardLayout.setBackgroundColor(Color.parseColor("#ffffff"));
+            viewHolder.leaderBoardShareButton.setVisibility(View.GONE);
         }
         viewHolder.rankTV.setText(String.valueOf(position+1));
         viewHolder.firstNameTV.setText(leaderBoardUserDetails.get(position).getName());
         viewHolder.scoreTV.setText(String.valueOf(leaderBoardUserDetails.get(position).getScore()));
+
+        viewHolder.leaderBoardShareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                context.startActivity(new ShareService("Look at where I am on the leader board", "I have completed " + String.valueOf(leaderBoardUserDetails.get(position).getScore()) + " moves this week. I am in " + String.valueOf(position+1) + positionPrefix(position+1) + " place. Can you beat me next week? \n Download the Nimble Fitness Companion app now: https://www-users.york.ac.uk/~hew550/", "Share your position on the leader board - " ).ShareContent());
+            }
+        });
+
 
         return convertView;
     }
@@ -75,5 +88,22 @@ public class LeaderboardListAdapter extends BaseAdapter {
         TextView firstNameTV;
         TextView scoreTV;
         LinearLayout leaderBoardLayout;
+        ImageView leaderBoardShareButton;
+    }
+
+    public String positionPrefix(int dayOfMonth){
+        switch (dayOfMonth){
+            case 1:
+            case 21:
+            case 31:
+                return ("st");
+            case 2:
+            case 22:
+                return ("nd");
+            case 3:
+            case 23:
+                return ("rd");
+            default: return ("th");
+        }
     }
 }
