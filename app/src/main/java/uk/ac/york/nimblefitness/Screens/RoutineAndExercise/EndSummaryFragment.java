72b1e1583b6d6ba5d6bf38f838677215f74a8112
wm758 <60884198+wm758@users.noreply.github.com>
2021-06-09 21:38:40 +0100

    Fixed faves fragment

--- app/src/main/java/uk/ac/york/nimblefitness/Screens/RoutineAndExercise/EndSummaryFragment.java
+++ app/src/main/java/uk/ac/york/nimblefitness/Screens/RoutineAndExercise/EndSummaryFragment.java
@@ -5,29 +5,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.Fragment;
-
-import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.FrameLayout;
-import android.widget.LinearLayout;
 import android.widget.RatingBar;
-import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.ToggleButton;
-import android.widget.Toolbar;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -36,11 +24,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.gson.Gson;
 
 import java.util.ArrayList;
 
-import uk.ac.york.nimblefitness.HelperClasses.CreateNotification;
 import uk.ac.york.nimblefitness.HelperClasses.Routine;
 import uk.ac.york.nimblefitness.MediaHandlers.Images.ImageLayout;
 import uk.ac.york.nimblefitness.R;
@@ -140,8 +126,6 @@ public class EndSummaryFragment extends Fragment {
         favouritesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i("onCheckedChanged", "onCheckedChanged: ");
-                Log.i("favouritesButton", (String) favouritesButton.getText());
                 if(favouritesButton.getText().equals("Add routine to favourites")) {
                     favouritesButton.setEnabled(false);
                     addNewFavouriteRoutine(favoriteRoutines);
@@ -156,7 +140,7 @@ public class EndSummaryFragment extends Fragment {
         mDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                Log.i("onDataChange", "onDataChange: ");
+                favoriteRoutines.clear();
                 for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
                     if (dataSnapshot != null) {
                         favoriteRoutines.add(dataSnapshot.getValue(String.class));
@@ -178,14 +162,9 @@ public class EndSummaryFragment extends Fragment {
     }
 
     private void removeFavouriteRoutine(ArrayList<String> favoriteRoutines) {
-        Log.i("removeFavouriteRoutine", favoriteRoutines.get(0));
-        Log.i("size", String.valueOf(favoriteRoutines.size()));
         for (int i = 0; i < favoriteRoutines.size(); i++) {
             if (favoriteRoutines.get(i).equals(routine.getRoutineName())) {
-                Gson gson = new Gson();
-                Log.i("before remove Routine", gson.toJson(favoriteRoutines));
                 favoriteRoutines.remove(i);
-                Log.i("after", gson.toJson(favoriteRoutines));
                 mDatabase.setValue(favoriteRoutines);
             }
         }
@@ -202,8 +181,6 @@ public class EndSummaryFragment extends Fragment {
 
             favoriteRoutines.add(routine.getRoutineName());
 
-            Log.i("addNewFavouriteRoutine", favoriteRoutines.get(favoriteRoutines.size() - 1));
-            Log.i("size", String.valueOf(favoriteRoutines.size()));
             mDatabase.setValue(favoriteRoutines);
         }
     }
