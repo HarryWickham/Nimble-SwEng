c77c44df54bf19f060e498b2116e01c61cbd22a1
willMills <wlm505@york.ac.uk>
2021-06-09 15:50:57 +0100

    Formatting and commenting

--- app/src/main/java/uk/ac/york/nimblefitness/HelperClasses/Routine.java
+++ app/src/main/java/uk/ac/york/nimblefitness/HelperClasses/Routine.java
@@ -1,19 +1,26 @@
 package uk.ac.york.nimblefitness.HelperClasses;
 
-import android.content.res.Resources;
-import android.graphics.Color;
-
 import java.io.Serializable;
-import java.sql.Struct;
 import java.util.ArrayList;
 
-import uk.ac.york.nimblefitness.MediaHandlers.Graphics.ShapeType;
-import uk.ac.york.nimblefitness.MediaHandlers.Images.ImageLayout;
-import uk.ac.york.nimblefitness.MediaHandlers.Text.TextLayout;
-import uk.ac.york.nimblefitness.MediaHandlers.Text.TextModule;
-import uk.ac.york.nimblefitness.MediaHandlers.Video.VideoLayout;
-import uk.ac.york.nimblefitness.R;
-
+/**
+ * A routine is a class that holds all the data for a whole routine
+ * One of the main fields in this class is the exerciseArrayList,
+ *  it has data for all exercises in the routine
+ * The routine is Serializable so that all the information can get transferred
+ *  between presentation pages
+ * The data for all the fields is set up in the routines.xml and loaded in routineFragment using RoutineData
+ * routineImage: Image for the routine, displayed in the RoutineFragment
+ * routineName: Name for the routine
+ * routineSummary: Short descriptive tag that shows in StartSummaryFragment
+ * rating: A rating for the routine that the user can give at the end of a routine
+ * sets: Number of sets the routine has
+ * restBetweenSets: Amount of seconds a user can rest between each set
+ * setsRemaining: A count for number of sets the user has gone through,
+ *  used to get to FinishFragment from EndSummaryFragment
+ * currentExercise: The current exercise the user is completing
+ * exerciseArrayList: List of all exercises in the routine
+ */
 public class Routine implements Serializable {
     private String routineImage;
     private String routineName;
@@ -25,7 +32,8 @@ public class Routine implements Serializable {
     private int currentExercise;
     private ArrayList<Exercise> exerciseArrayList;
 
-    public Routine(String routineImage, String routineName, String routineSummary, int rating, int sets, int restBetweenSets, int setsRemaining, int currentExercise, ArrayList<Exercise> exerciseArrayList) {
+    public Routine(String routineImage, String routineName, String routineSummary, int rating, int sets,
+                   int restBetweenSets, int setsRemaining, int currentExercise, ArrayList<Exercise> exerciseArrayList) {
         this.routineImage = routineImage;
         this.routineName = routineName;
         this.routineSummary = routineSummary;
@@ -34,13 +42,6 @@ public class Routine implements Serializable {
         this.restBetweenSets = restBetweenSets;
         this.setsRemaining = setsRemaining;
         this.currentExercise = currentExercise;
-        this.exerciseArrayList = exerciseArrayList;
-    }
-
-    public Routine(String routineImage, String routineName, int sets, ArrayList<Exercise> exerciseArrayList) {
-        this.routineImage = routineImage;
-        this.routineName = routineName;
-        this.sets = sets;
         this.exerciseArrayList = exerciseArrayList;
     }
 
@@ -117,223 +118,5 @@ public class Routine implements Serializable {
 
     public void setCurrentExercise(int currentExercise) {
         this.currentExercise = currentExercise;
-    }
-
-    public Routine getExampleRoutine(){
-        ArrayList<Exercise> exercises = new ArrayList<>();
-        ArrayList<ShapeType> shapeTypes = new ArrayList<>();
-
-        int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
-        final double imageAspectRatio = 1.2456; // Change to -> 1.4528 'Preserves the image's original aspect ratio 600:413'.
-        final double videoAspectRatio = 0.5625; // Ensures the video has a 16:9 aspect ratio.
-        final double titleScaling = 0.03; // Scales the title font size depending on screen size.
-        final double descriptionScaling = 0.0118; // Scales the description text font size depending on screen size.
-        int videoWidth = screenWidth - 100; // The '-100' accounts for the rectangle 'border' behind the video.
-        int imageAndVideoHeight = (int) Math.round(videoWidth * videoAspectRatio); // Common height of image and video.
-        int imageWidth = (int) Math.round(imageAndVideoHeight * imageAspectRatio);
-        int videoYstart = 100 + 5*(int) Math.round(screenWidth * titleScaling); // whitespace above and below title + size of font.
-        int imageYstart = videoYstart + imageAndVideoHeight + 75;
-        int descriptionYstart = imageYstart + imageAndVideoHeight + 75; // 75's tak into account the borders and element spacing.
-        String titleFontSize = Integer.toString((int) Math.round(screenWidth * titleScaling));
-        String descriptionFontSize = Integer.toString((int) Math.round(screenWidth * descriptionScaling));
-
-        shapeTypes.clear();
-        shapeTypes.add(new ShapeType(25,videoYstart - 25,imageAndVideoHeight + 50,screenWidth - 50,Color.parseColor("#303F9F"),"RECTANGLE",0)); //video box
-        shapeTypes.add(new ShapeType(25,imageYstart - 25,imageAndVideoHeight + 50,imageWidth + 50,Color.parseColor("#303F9F"),"RECTANGLE",0)); //Image box
-
-        ArrayList<TextLayout> textLayouts = new ArrayList<>();
-        textLayouts.add(new TextLayout("Push Ups",
-                TextModule.fontFamily.default_bold,
-                titleFontSize,
-                "#000000",
-                50,
-                50,
-                null,
-                null));
-        textLayouts.add(new TextLayout("With your hands placed a shoulder width apart and a straight back, lower yourself to the ground keeping your elbows tucked in. Hold the position. Then push off of the floor to your start position to complete a rep.",
-                TextModule.fontFamily.sans_serif,
-                descriptionFontSize,
-                "#000000",
-                50,
-                descriptionYstart,
-                null,
-                null));
-
-        exercises.add(new Exercise(
-                new ImageLayout(50,
-                imageYstart,
-                imageWidth,
-                imageAndVideoHeight,
-                0,
-                "https://www-users.york.ac.uk/~hew550/NimbleAssets/exercisemusclegroups/normal_push_up.png",
-                null,
-                null),
-                shapeTypes,
-                "Push Ups",
-                "With your hands placed a shoulder width apart and a straight back, lower yourself to the ground keeping your elbows tucked in. Hold the position. Then push off of the floor to your start position to complete a rep.",
-                "number",
-                5,
-                5,
-                1,
-                30,
-                Color.parseColor("#008080"),
-                new VideoLayout("https://www-users.york.ac.uk/~hew550/NimbleAssets/exercisevideos/Normal-Push-Up.mp4",
-                        videoWidth,
-                        imageAndVideoHeight,
-                        50,
-                        videoYstart,
-                        "",
-                        0,
-                        true,
-                        null,
-                        null),
-                textLayouts,
-                null));
-
-        ArrayList<TextLayout> textLayouts2 = new ArrayList<>();
-        textLayouts2.add(new TextLayout("Plank",
-                TextModule.fontFamily.default_bold,
-                titleFontSize,
-                "#000000",
-                50,
-                50,
-                null,
-                null));
-        textLayouts2.add(new TextLayout("From a normal push up position, lower yourself down so that your weight is resting on your forearms. With a straight back, hold this position by engaging your core muscles.",
-                TextModule.fontFamily.sans_serif,
-                descriptionFontSize,
-                "#000000",
-                50,
-                descriptionYstart,
-                null,
-                null));
-
-        exercises.add(new Exercise(new ImageLayout(50,
-                imageYstart,
-                imageWidth,
-                imageAndVideoHeight,
-                0,
-                "https://www-users.york.ac.uk/~hew550/NimbleAssets/exercisemusclegroups/plank.png",
-                null,
-                null),
-                shapeTypes,
-                "Plank",
-                "From a normal push up position, lower yourself down so that your weight is resting on your forearms. With a straight back, hold this position by engaging your core muscles.",
-                "time",
-                20,
-                0,
-                1,
-                30,
-                Color.parseColor("#FF2400"),
-                new VideoLayout("https://www-users.york.ac.uk/~hew550/NimbleAssets/exercisevideos/Plank.mp4",
-                        videoWidth,
-                        imageAndVideoHeight,
-                        50,
-                        videoYstart,
-                        "",
-                        0,
-                        true,
-                        null,
-                        null),
-                textLayouts2,
-                null));
-
-        ArrayList<TextLayout> textLayouts3 = new ArrayList<>();
-        textLayouts3.add(new TextLayout("Tricep Dip",
-                TextModule.fontFamily.sans_serif,
-                titleFontSize,
-                "#000000",
-                50,
-                50,
-                null,
-                null));
-        textLayouts3.add(new TextLayout("Using a chair, put your weight onto your hands then lower yourself slowly down so that your legs are straight and your body forms an 'L' shape. Push off of the chair to return to your start position to complete a rep.",
-                TextModule.fontFamily.sans_serif,
-                descriptionFontSize,
-                "#000000",
-                50,
-                descriptionYstart,
-                null,
-                null));
-
-        exercises.add(new Exercise(new ImageLayout(50,
-                imageYstart,
-                imageWidth,
-                imageAndVideoHeight,
-                0,
-                "https://www-users.york.ac.uk/~hew550/NimbleAssets/exercisemusclegroups/tricep_dip.png",
-                null,
-                null),
-                shapeTypes,
-                "Tricep Dip",
-                "Using a chair, put your weight onto your hands then lower yourself slowly down so that your legs are straight and your body forms an 'L' shape. Push off of the chair to return to your start position to complete a rep.",
-                "number",
-                5,
-                5,
-                1,
-                30,
-                Color.parseColor("#FFDB58"),
-                new VideoLayout("https://www-users.york.ac.uk/~hew550/NimbleAssets/exercisevideos/Tricep-Dip.mp4",
-                        videoWidth,
-                        imageAndVideoHeight,
-                        50,
-                        videoYstart,
-                        "",
-                        0,
-                        true,
-                        null,
-                        null),
-                textLayouts3,
-                null));
-
-        ArrayList<TextLayout> textLayouts4 = new ArrayList<>();
-        textLayouts4.add(new TextLayout("Superman",
-                TextModule.fontFamily.default_bold,
-                titleFontSize,
-                "#000000",
-                50,
-                50,
-                null,
-                null));
-        textLayouts4.add(new TextLayout("Start by laying on your front. Raise your legs off of the floor while simultaneously raising your arms off of the floor using your shoulder and back muscles. Hold this position.",
-                TextModule.fontFamily.sans_serif,
-                descriptionFontSize,
-                "#000000",
-                50,
-                descriptionYstart,
-                null,
-                null));
-
-        exercises.add(new Exercise(new ImageLayout(50,
-                imageYstart,
-                imageWidth,
-                imageAndVideoHeight,
-                0,
-                "https://www-users.york.ac.uk/~hew550/NimbleAssets/exercisemusclegroups/superman.png",
-                null,
-                null),
-                shapeTypes,
-                "Superman",
-                "Start by laying on your front. Raise your legs off of the floor while simultaneously raising your arms off of the floor using your shoulder and back muscles. Hold this position.",
-                "number",
-                5,
-                4,
-                1,
-                30,
-                Color.parseColor("#BFFF00"),
-                new VideoLayout("https://www-users.york.ac.uk/~hew550/NimbleAssets/exercisevideos/Superman.mp4",
-                        videoWidth,
-                        imageAndVideoHeight,
-                        50,
-                        videoYstart,
-                        "",
-                        0,
-                        true,
-                        null,
-                        null),
-                textLayouts4,
-                null));
-
-        return new Routine("https://www-users.york.ac.uk/~hew550/NimbleAssets/routineimages/beginUp.png","Beginners Upper Body","This is a test routine to ensure all routine related pages take the correct information from this single object",3,4, 60, 4,0, exercises);
     }
 }
